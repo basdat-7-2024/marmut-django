24830a7edf2eb04c2acdbf47356a510dca2c319a
aryawijayak <aryawijayak2004@gmail.com>
2024-05-15 19:16:39 +0700

    fix link search page

--- searchbar/views.py
+++ searchbar/views.py
@@ -1,7 +1,6 @@
 # views.py
 from django.db import connection
 from django.shortcuts import render
-from django.utils.html import escape
 from .query import get_search_query  # Ensure this import reflects your project structure
 
 def searchpage(request, query):
@@ -9,7 +8,6 @@ def searchpage(request, query):
     return render(request, 'searchpage.html', {'query': query, 'results': results})
 
 def perform_search(query):
-    # Fetch the SQL query from query.py
     search_query = get_search_query()
     with connection.cursor() as cursor:
         search_pattern = '%' + query + '%'
