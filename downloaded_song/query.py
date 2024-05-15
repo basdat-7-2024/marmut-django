756f0855a547812d260233e417a887792b8a3873
aryawijayak <aryawijayak2004@gmail.com>
2024-05-15 13:47:26 +0700

    finished delete downloaded song

--- downloaded_song/query.py
+++ downloaded_song/query.py
@@ -1,7 +1,7 @@
 def get_downloaded_song(email):
     return f"""
         SELECT 
-            k.judul AS Judul
+            k.id, k.judul
         FROM 
             downloaded_song d
             JOIN song s ON d.id_song = s.id_konten
@@ -10,4 +10,18 @@ def get_downloaded_song(email):
             d.email_downloader = '{email}'
         ORDER BY 
             k.judul ASC;
+    """
+
+
+def delete_downloaded_song(email, song_id):
+    return f"""
+        BEGIN;
+        DELETE FROM downloaded_song 
+        WHERE email_downloader = '{email}' 
+        AND id_song = '{song_id}';
+        
+        UPDATE song 
+        SET total_download = total_download - 1 
+        WHERE id_konten = '{song_id}';
+        COMMIT;
     """
