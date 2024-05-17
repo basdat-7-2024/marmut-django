b23ddc04b5abdc5634ed1445a80c6914c1114ffd
cnotgate <49557224+cnotgate@users.noreply.github.com>
2024-05-17 17:06:28 +0700

    shuffle play

---
+++ user_playlist/query.py
@@ -0,0 +1,15 @@
+import datetime
+
+
+def get_playlist_info_from_id(id):
+    return f"select email_pembuat, id_user_playlist, judul, jumlah_lagu, tanggal_dibuat, id_playlist, total_durasi, nama, deskripsi from (SELECT * FROM user_playlist WHERE id_playlist = \'{id}\') t1 left join (select email, nama from akun) t2 on t1.email_pembuat=t2.email;"
+
+def get_playlist_songs_from_id(id):
+    return f"SELECT id,judul,durasi, (SELECT nama from akun where email in (SELECT email_akun from artist where id in (SELECT id_artist FROM song WHERE id_konten in (select id_song from playlist_song where id_playlist=\'{id}\')))) as artist FROM konten WHERE id IN (select id_song from playlist_song where id_playlist=\'{id}\')"
+
+def insert_into_akun_play_user_playlist(email_pemain, id_user_playlist, email_pembuat):
+    # email_pemain id_user_playlist email_pembuat waktu
+    timezone_offset = 0  # UTC
+    tzinfo = datetime.timezone(datetime.timedelta(hours=timezone_offset))
+    time_now = datetime.now(tzinfo)
+    return f"insert into akun_play_user_playlist (email_pemain, id_user_playlist, email_pembuat, waktu) values (\'{email_pemain}\', \'{id_user_playlist}\', \'{email_pembuat}\', \'{time_now}\')"
--- user_playlist/views.py
+++ user_playlist/views.py
@@ -1,6 +1,66 @@
+from django.db import connection
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
+from user_playlist.query import *
+from user_playlist.helper import *
 
 # Create your views here.
 
 def user_playlist_detail(request):
-    return render(request, 'playlistdetail_by_user.html')
+    id = request.GET.get('id')
+    cursor_info = connection.cursor()
+    cursor_lagu = connection.cursor()
+    if(not is_valid_uuid(id)):
+        raise Http404
+    
+    cursor_info.execute(get_playlist_info_from_id(id))
+    cursor_lagu.execute(get_playlist_songs_from_id(id))
+    playlist_info_raw = cursor_info.fetchall()[0]
+    lagu_in_playlist_raw = cursor_lagu.fetchall()
+
+    if(playlist_info_raw == []):
+        raise Http404
+    
+    lagu_in_playlist = []
+    playlist_info = {}
+    
+    playlist_info['email_pembuat'] = playlist_info_raw[0]
+    playlist_info['judul'] = playlist_info_raw[2]
+    playlist_info['jumlah_lagu'] = playlist_info_raw[3]
+    playlist_info['tanggal_dibuat'] = playlist_info_raw[4]
+    playlist_info['id_playlist'] = playlist_info_raw[5]
+    playlist_info['total_durasi'] = f"{playlist_info_raw[6]} Menit" if playlist_info_raw[6] < 60 else f"{playlist_info_raw[6]//60} Jam {playlist_info_raw[6]%60} Menit"
+    playlist_info['nama'] = playlist_info_raw[7]
+    playlist_info['deskripsi'] = playlist_info_raw[8]
+
+    
+
+    for i in lagu_in_playlist_raw:
+        temp = {}
+        id_song = i[0]
+        judul = i[1]
+        durasi = i[2]
+        artist = i[3]
+        temp['id_song'] = id_song
+        temp['judul'] = judul
+        temp['durasi'] = f"{durasi} Menit" if durasi < 60 else f"{durasi//60} Jam {durasi%60} Menit"
+        temp['artist'] = artist
+        lagu_in_playlist.append(temp)
+
+    context = {
+        'playlist_info': playlist_info,
+        'songs':lagu_in_playlist
+    }
+    # print(cursor.fetchall())
+    return render(request, 'playlistdetail_by_user.html', context)
+
+def shuffle_play(request):
+    cursor = connection.cursor()
+
+    email_pemain = request.session.get('email')
+    id_playlist = request.GET.get('id')
+    email_pembuat = request.GET.get('email_pembuat')
+    print(f'{email_pemain} {id_playlist} {email_pembuat}')
+
+    # cursor.execute(insert_into_akun_play_user_playlist(email_pemain, id_playlist, email_pembuat))
+    return HttpResponse('OK')
