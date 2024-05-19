cff3df3e3e13c3885dfb5212c0bfc33a8ab4f7d5
cnotgate <49557224+cnotgate@users.noreply.github.com>
2024-05-19 20:14:17 +0700

    commit dlu

--- kelola_playlist/urls.py
+++ kelola_playlist/urls.py
@@ -6,5 +6,7 @@ app_name = 'kelola_playlist'
 
 urlpatterns = [
     path('', kelola_playlist, name='kelola_playlist'),
-    path('playlist-detail/', playlist_detail, name='playlist_detail'),
+    path('detail/<str:id>', playlist_detail, name='playlist_detail'),
+    path('ubah-playlist', ubah_playlist, name="ubah_playlist"),
+    path('hapus-playlist/<str:id>', hapus_playlist, name="hapus_playlist"),
 ]
--- searchbar/urls.py
+++ searchbar/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from searchbar.views import *
+from user_playlist.views import *
 
 
 app_name = 'searchbar'
 
 urlpatterns = [
     path('<str:query>/', searchpage, name='searchpage'),
+    path('playlist/<str:id>/', user_playlist_detail, name='user_playlist_detail'),
 ]
